bfac305a36de90b9b52104a0d9a0454f5bc1551d
geekelo <efuelight12@gmail.com>
2023-06-14 20:23:28 +0100

    fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -11,24 +11,24 @@ module.exports = {
   devServer: {
     static: './dist',
   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       title: 'Output Management',
-       template: './src/index.html',
-     }),
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+   clean: true,
+  },
+  module: {
+   rules: [
+     {
+       test: /\.css$/i,
+       use: ['style-loader', 'css-loader'],
+     },
    ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-   },
-   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
   },
   optimization: {
     runtimeChunk: 'single',
