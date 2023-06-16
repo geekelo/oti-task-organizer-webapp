e93d94b46743392ce7fab0a4194baa906093e521
geekelo <efuelight12@gmail.com>
2023-06-16 19:38:08 +0100

    Fix linter errors

--- src/print.js
+++ src/print.js
@@ -37,7 +37,7 @@ export default class PrintMe {
         });
         localStorage.setItem('storedTasks', JSON.stringify(this.addedTasks));
         this.displayTasks();
-      },);
+      });
   }
 
   updateTaskDescription() {
